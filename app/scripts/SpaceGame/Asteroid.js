6d897a8b3bec55951488813f3cbcee194f5e668c
Nicholas Wertzberger <wertnick@gmail.com>
2014-04-05 14:36:23 -0500

    Asteroids bounce off each other. They don't split yet.

--- app/scripts/SpaceGame/Asteroid.js
+++ app/scripts/SpaceGame/Asteroid.js
@@ -1,34 +1,89 @@
-(function (SpaceGame) {
+(function (SpaceGame, Phaser) {
     "use strict";
-    SpaceGame.Asteroid = function(game, initialVelocity, initialSize) {
+    SpaceGame.Asteroid = function(game, asteroids, initialPosition, initialVelocity, initialSize) {
         var self = this;
-        self.asteroid = game.add.sprite(
-            game.width / 2,
-            game.height / 2,
-            'asteroid');
+        self.asteroid = asteroids.create(
+            initialPosition.x,
+            initialPosition.y,
+            'asteroid'
+        );
 
         self.asteroid.scale = {
             x: initialSize,
             y: initialSize
         };
 
+        self.initialSize = initialSize;
+
         self.asteroid.anchor.setTo(.5,.5);
 
-        game.physics.enable(self.asteroid, Phaser.Physics.ARCADE);
+        game.physics.enable(self.asteroid);
         self.asteroid.body.allowRotation = true;
         self.asteroid.body.angularVelocity = 100;
-        self.asteroid.body.collideWorldBounds = true;
+        self.asteroid.body.inertia = 1.0;
+        self.asteroid.body.bounce = {x: 1.0, y : 1.0};
 
         self.asteroid.body.velocity = initialVelocity;
 
         self.update = function() {
-            game.physics.collide(self.asteroid, function() {
-                new SpaceGame.Asteroid(game, {
-                    x: -self.asteroid.velocity.x,
-                    y: -self.asteroid.velocity.y
-                },
-                initialSize / 2);
-            }, null, this);
+            if (self.asteroid) {
+                self.wrapAtBounds();
+            }
+            if (self.childAsteroid1)
+                self.childAsteroid1.update();
+            if (self.childAsteroid2)
+                self.childAsteroid2.update();
+
+        };
+        self.wrapAtBounds = function() {
+            var roidBody = self.asteroid.body;
+            var left = -self.asteroid.width / 2;
+            var right = game.width + self.asteroid.width / 2;
+            var top = -self.asteroid.height / 2;
+            var bottom = game.height + self.asteroid.height / 2;
+            if(roidBody.x > right) {
+                roidBody.x = left;
+            }
+            else if(roidBody.x < left) {
+                roidBody.x = right;
+            }
+            else if (roidBody.y > bottom) {
+                roidBody.y = top;
+            }
+            if (roidBody.y < top) {
+                roidBody.y = bottom;
+            }
         };
     };
-})(SpaceGame);
+
+    SpaceGame.Asteroid.processAsteroid = function(asteroid, object) {
+        if (asteroid.initialSize < 0.25) {
+            asteroid.destroy();
+            return;
+        }
+
+        new SpaceGame.Asteroid(
+            game,
+            asteroids,
+            asteroid, {
+                x: object.body.velocity.x * 2,
+                y: object.body.velocity.y * 2
+            },
+            initialSize / 2
+        );
+        new SpaceGame.Asteroid(
+            asteroids,
+            asteroid, {
+                x: object.body.velocity.x * 2,
+                y: object.body.velocity.y * 2
+            },
+            initialSize / 2
+        );
+        asteroid.destroy();
+    };
+
+    SpaceGame.Asteroid.split = function(asteroid1, asteroid2) {
+        SpaceGame.Asteroid.processAsteroid(asteroid1, asteroid2);
+        SpaceGame.Asteroid.processAsteroid(asteroid2, asteroid1);
+    };
+})(SpaceGame, Phaser);
--- app/scripts/SpaceGame/Level.js
+++ app/scripts/SpaceGame/Level.js
@@ -8,13 +8,18 @@ window.developingState = "Level";
             self.ship = game.add.sprite(50,50,'ship');
             self.hud = new Hud(game, self.ship);
             self.controls = new Controls(game, self.ship);
-            self.asteroid = new Asteroid(game, {x: 40, y: 40}, 2);
+            self.asteroids = game.add.group();
+            self.asteroid1 = new Asteroid(game, self.asteroids, {x:10, y : 10}, {x: 40, y: 40}, 2);
+            self.asteroid2 = new Asteroid(game, self.asteroids, {x:200, y: 10}, {x: -40, y: 40}, 2);
         };
 
         self.update = function() {
             self.hud.update(game);
             self.controls.update(game);
-            self.asteroid.update(game);
+            self.asteroid1.update(game);
+            self.asteroid2.update(game);
+            game.physics.arcade.collide(self.asteroids, self.asteroids);
+            
         };
     };
 })(SpaceGame, SpaceGame.Hud, SpaceGame.Controls, SpaceGame.Asteroid);
